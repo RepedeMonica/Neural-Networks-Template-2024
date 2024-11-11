d07612b4204d23ec195b738e310bb703dbe3f089
RepedeMonica <126000692+RepedeMonica@users.noreply.github.com>
2024-11-11 15:45:19 +0200

    Add files via upload

---
+++ Lab03/asignment3.py
@@ -0,0 +1,158 @@
+import numpy as np
+from torchvision.datasets import MNIST
+import time
+
+#1. Load the MNIST dataset
+def download_mnist(is_train: bool):
+    dataset = MNIST(
+        root='./data',
+        transform=lambda x: np.array(x).flatten(),
+        download=True,
+        train=is_train
+    )
+    mnist_data = []
+    mnist_labels = []
+    for image, label in dataset:
+        mnist_data.append(image)
+        mnist_labels.append(label)
+    return np.array(mnist_data), np.array(mnist_labels)
+train_X, train_Y = download_mnist(True)
+test_X, test_Y = download_mnist(False)
+
+#normalize the data
+train_X = train_X / 255.0
+test_X = test_X / 255.0
+
+#convert the labels to one-hot-encoding.
+train_Y_encoding = np.zeros((train_Y.size, 10))
+train_Y_encoding[np.arange(train_Y.size), train_Y] = 1
+
+test_Y_encoding = np.zeros((test_Y.size, 10))
+test_Y_encoding[np.arange(test_Y.size), test_Y] = 1
+
+#x 60 000* 784
+#784 input neurons, 100 hidden neurons, and 10 output neurons
+np.random.seed(40)
+def initialization():
+    #weight intialization cu xavier uniform , slide 35 curs 6
+    limit_W1 = np.sqrt(6/ (784 + 100))
+    W1 = np.random.uniform(-limit_W1, limit_W1, (784, 100))
+    b1 = np.zeros((1, 100))
+
+    limit_W2 = np.sqrt(6 / (100 + 10))
+    W2 = np.random.uniform(-limit_W2, limit_W2, (100, 10))
+    b2 = np.zeros((1, 10))
+
+    return W1, b1, W2, b2
+W1, b1, W2, b2 = initialization()
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def softmax(z):
+    exp_z = np.exp(z - np.max(z, axis=1, keepdims=True)) #slide 30 curs 5
+    return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+def forward(X, W1, b1, W2, b2, dropout_rate=0.2, training = True): #80% raman activi
+    #slide 35 curs 4 sau pseudocodul generalizat-48 curs 5
+    Z1 = np.dot(X, W1) + b1
+    A1 = sigmoid(Z1)
+
+    if training:
+        dropout_mask = (np.random.rand(*A1.shape) > dropout_rate).astype(float)  #masca dropout
+        A1 *= dropout_mask  # dezactivam neuronii
+        A1 /= (1 - dropout_rate)
+
+    Z2 = np.dot(A1, W2) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def cross_entropy(t_true, y_pred):
+    return -np.mean(np.sum(t_true * np.log(y_pred), axis=1)) #slide 21 curs 5
+
+def backward(X, Y, A1, A2, W1, W2 , b1, b2, learning_rate=0.01):
+    m = X.shape[0]
+
+    #slide 36,curs5 -dem pt formula
+    dZ2 = A2 - Y  #eroarea pt final layer, formula slide 44, curs5 #derivata softmax
+    dW2 = np.dot(A1.T, dZ2)  / m   #formula slide 46 - formula basic de calculare a w
+    db2 = np.sum(dZ2, axis=0, keepdims=True) / m  #slide 47 zice ca e dZ2 ul
+
+    # gradient pentru stratul ascuns
+    dA1 = np.dot(dZ2, W2.T)  #eroarea propagata, slide 45
+    dZ1 = dA1 * A1 * (1 - A1)  #eroarea finala= eroarea propagata*deriv sigm;  deriv(sigm)=sigm*(1-sigm)(slide 87,curs4)
+    # slide 6, curs 5
+    dW1 = np.dot(X.T, dZ1) / m
+    db1 = np.sum(dZ1, axis=0, keepdims=True) / m
+
+    # actualizare parametri conform gradient descent
+    #slide 83, curs4
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+
+    return W1, b1, W2, b2
+
+def accuracy(t_true, y_pred):
+    pred_label = np.argmax(y_pred, axis=1)
+    true_label = np.argmax(t_true, axis=1)
+    return np.mean(pred_label == true_label)
+
+def train(X, Y, W1, b1, W2, b2, epochs=200, batch_size=64, learning_rate=0.05):
+    patience = 3
+    decay_rate = 0.8
+    best_accuracy = 0
+    epochs_without_improvement = 0
+
+    m = X.shape[0]
+    epoch = 0
+    start_time = time.time()
+    max_time = 5*60
+    while epoch < epochs:
+        # shuffle data
+        indices = np.arange(m)
+        np.random.shuffle(indices)
+        X = X[indices]
+        Y = Y[indices]
+
+        for i in range(0, m, batch_size):
+            X_batch = X[i:i+batch_size]
+            y_batch = Y[i:i+batch_size]
+
+            Z1, A1, Z2, A2 = forward(X_batch, W1, b1, W2, b2, training=True)
+            W1, b1, W2, b2 = backward(X_batch, y_batch, A1, A2, W1, W2, b1, b2, learning_rate)
+
+        _, _, _, A2_train = forward(X, W1, b1, W2, b2)
+        _, _, _, A2_test = forward(test_X, W1, b1, W2, b2, training=False)
+
+        train_acc = accuracy(Y, A2_train)
+        test_acc = accuracy(test_Y_encoding, A2_test)
+
+        elapsed_time = time.time() - start_time
+        print(f"Epoch {epoch+1}/{epochs} - Train Accuracy: {train_acc*100:.2f}% - Test Accuracy: {test_acc*100:.2f}%  - Elapsed Time: {elapsed_time:.2f}")
+       # if train_acc >= 0.95 and test_acc >= 0.95:
+        #    print("Stop the accuracy is over 95")
+        #    break
+        if elapsed_time >= max_time:
+            print("Stop due to time limit")
+            break
+
+        if test_acc > best_accuracy:
+            best_accuracy = test_acc
+            epochs_without_improvement = 0
+        else:
+            epochs_without_improvement += 1
+        if epochs_without_improvement >= patience:
+            learning_rate = learning_rate * decay_rate
+            epochs_without_improvement = 0
+            print(f"Reducing learning rate to {learning_rate}")
+        epoch +=1
+
+    return W1, b1, W2, b2
+
+W1_trained, b1_trained, W2_trained, b2_trained = train(train_X, train_Y_encoding, W1, b1, W2, b2)
+
+
+
+
